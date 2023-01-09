a689ce975dcaef684697805d9b846c5a8450b795
miguelToscano <miguetoscano288@gmail.com>
2023-01-09 00:51:34 -0300

    cargo fmt

--- w3ns/src/application/canister_methods.rs
+++ w3ns/src/application/canister_methods.rs
@@ -52,9 +52,7 @@ pub async fn send_sms(input: SendSmsInput) -> Result<(), ApiError> {
 
 #[update]
 #[candid_method(update)]
-pub async fn send_push_notification(
-    input: SendPushInput
-) -> Result<(), ApiError> {
+pub async fn send_push_notification(input: SendPushInput) -> Result<(), ApiError> {
     let caller = ic::caller();
     let api_key = api_keys_service::validate_api_key(&caller)?;
     push_service::send_courier_push(&api_key.value, &input).await
@@ -89,9 +87,7 @@ pub fn delete_topic(topic_name: String) -> Result<(), ApiError> {
 
 #[update]
 #[candid_method(update)]
-pub async fn send_push_to_topic(
-    input: SendPushToTopicInput
-) -> Result<(), ApiError> {
+pub async fn send_push_to_topic(input: SendPushToTopicInput) -> Result<(), ApiError> {
     let caller = ic::caller();
     let api_key = api_keys_service::validate_api_key(&caller)?;
     topics_service::get_topic(&caller, input.clone().topic)?;
